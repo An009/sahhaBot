dabc52cc62730b0b93f3599c880040d947c73aae
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-07-14 23:59:16 +0100

    Fix Tailwind ring color generation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,7 +71,6 @@ const config: Config = {
           '3': 'hsl(var(--chart-3))',
           '4': 'hsl(var(--chart-4))',
           '5': 'hsl(var(--chart-5))',
-        },
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
